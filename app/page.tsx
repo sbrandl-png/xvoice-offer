8446fc07cf6bda04457eedd6d6350cc5da88abef
sbrandl-png <s.brandl@xvoice-uc.de>
2025-11-06 23:22:30 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-use client";
+"use client";
 import React, { useMemo, useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
